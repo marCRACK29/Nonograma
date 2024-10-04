76077e15f687d986b3fa23e9e14d15e211926a36
StaCKm29 <mamartinez2023@udec.cl>
2024-10-04 16:34:01 -0300

    Base de interfaz "State"
    Será la encargada de mandar las señales a las clases concretas; Inicio, ElegirTamaño, Jugar.

---
+++ src/State/State.py
@@ -0,0 +1,19 @@
+import Context
+from abc import ABC, abstractmethod
+
+class State(ABC):
+    @property
+    def context(self) -> Context:
+        return self._context
+
+    @context.setter
+    def context(self, context: Context) -> None:
+        self._context = context
+
+    @abstractmethod
+    def handle1(self) -> None:
+        pass
+
+    @abstractmethod
+    def handle2(self) -> None:
+        pass
