447e08d2844b15116f4ef8cb67317fde0d5c8a32
StaCKm29 <mamartinez2023@udec.cl>
2024-10-05 21:17:42 -0300

    refactor: Reducción de funciones de ToggleButton
    Pasar de dos funciones para el estado del botón del mouse a una sola función.

--- src/state/ToggleButton.py
+++ src/state/ToggleButton.py
@@ -1,9 +1,5 @@
 import pygame
 
-SCREEN_WIDTH  = 500
-SCREEN_HEIGHT  = 800
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 class ToggleButton:
     def __init__(self, x, y, image, scale):
         width = image.get_width()
@@ -13,20 +9,22 @@ class ToggleButton:
         self.rect.topleft = (x, y)
         self.clicked = False
 
-    def click(self):
+    def click(self)-> bool:
+        accion = False
         #obtener posición del mouse
         pos = pygame.mouse.get_pos()
+        presionado = pygame.mouse.get_pressed()[0] #para el botón izquierdo del mouse
         #verificar si el mouse está sobre el botón
         if self.rect.collidepoint(pos):
             #verificar si se hizo click en el botón izquierdo
-            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+            if presionado == 1 and self.accion == False:
                 self.clicked = True
+                accion = True
+            elif presionado == 0:
+                self.clicked = False
 
-    def desclick(self):
-        #Resetear clicked si el mouse no está presionado
-        if pygame.mouse.get_pressed()[0] == 0:
-            self.clicked = False
+        return accion
 
-    def draw(self):
-        screen.blit(self.image, (self.rect.x, self.rect.y))
+    def draw(self, surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
 
