9472c44177a0866f3c3d95535988c05d2df5c95b
Felicia <feliciapino100@gmail.com>
2024-11-28 13:49:40 -0300

    merge

--- tests/test_vidas.py
+++ tests/test_vidas.py
@@ -2,16 +2,13 @@ import pytest
 from src.vidas import *
 
 def test_createHealthCounter():
-    hp = HP_counter(3,50,(100,200))
+    hp = HP_counter(3)
     assert hp.maxLives == 3
     assert hp.lives == 3
-    assert hp.scale == 50
-    assert hp.pos[0] == 100
-    assert hp.pos[1] == 200
 
 
 def test_healthCounter():
-    hp = HP_counter(4,30,(50,50))
+    hp = HP_counter(4)
     assert hp.lives == hp.maxLives
     hp.loseLife()
     assert hp.lives == 3
